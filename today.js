d3dcf3f19d7a4e93a6c1e57d6635791a40b7c02c
NandaMohan <92676605+NandaMohan@users.noreply.github.com>
2021-12-09 18:28:52 +0530

    Create today.js

---
+++ today.js
@@ -0,0 +1,39 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio')
+// const requestOptions = {
+//   method: 'GET',
+//   uri : 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+//   headers: {
+//     'X-CMC_PRO_API_KEY': '0a18e6f7-a23a-4986-bf34-25c12efcd494'
+//   },
+//   json: true,
+//   gzip: true
+// };
+
+// rp(requestOptions).then(response => {
+//   //console.log('API call response:', response.data);
+
+//   if(response.data && Array.isArray(response.data)) {
+//      let btc = response.data.filter(crypto => crypto.symbol === 'BTC' );
+//      console.log(btc[0].quote)
+//   }
+
+// }).catch((err) => {
+//   console.log('API call error:', err.message);
+// });
+
+
+rp("https://www.bullion-rates.com/gold/INR-history.htm", (error, response, html) => {
+    if(!error && response.statusCode==200) {
+        const $= cheerio.load(html);
+
+        const datarow= $(".HeaderRow");
+        const output= datarow.find("th").text();
+        $(".DataRow").each((i, data) => {
+            const item= $(data). text();
+
+            console.log(item);
+        })
+    }
+
+});
