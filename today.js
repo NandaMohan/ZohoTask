e1b7cebeda8f9ccd6d8ee4cd06e99fa3cedbb4b7
NandaMohan <92676605+NandaMohan@users.noreply.github.com>
2021-12-10 18:32:24 +0530

    Update today.js

--- today.js
+++ today.js
@@ -1,5 +1,7 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio')
+const nodemailer = require('nodemailer');
+
 // const requestOptions = {
 //   method: 'GET',
 //   uri : 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
@@ -23,15 +25,34 @@ const cheerio = require('cheerio')
 // });
 
 
-rp("https://www.bullion-rates.com/gold/INR-history.htm", (error, response, html) => {
+
+
+let columnNames = ["date", "price", "gram"];
+let item = []
+
+console.log("fetching...")
+
+rp("https://www.searchindia.info/exchange-rates/todays-gold-price.html", (error, response, html) => {
     if(!error && response.statusCode==200) {
         const $= cheerio.load(html);
 
-        $(".DataRow").each((i, data) => {
-            const item= $(data). text();
+        $(".table.table-striped.table-bordered.list-rates tr").each((i, data) => {
+            const cells = $("td", data);
+            let row = {}
+            
+            cells.each((i, td) => {
+                
+                let text = $(td).text().trim();    
+                row[columnNames[i]] = text
+                
+            })
+            item.push(row)
+        
+            // // let store = [item.replace('\t','')]
+            // console.log(item)
+        });
 
-            console.log(item);
-        })
+        console.log(item)
     }
 
 });
