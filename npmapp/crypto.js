8b36fb22011dc62edc3426aa729d8e532087b694
NandaMohan <92676605+NandaMohan@users.noreply.github.com>
2021-12-20 15:36:15 +0530

    Update crypto.js

--- npmapp/crypto.js
+++ npmapp/crypto.js
@@ -1,7 +1,6 @@
 const rp = require('request-promise');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-var mail1 = require('./mail.js')
 
 const requestOptions = {
   method: 'GET',
@@ -25,37 +24,20 @@ rp(requestOptions).then(response1 => {
         USDTINR = parseFloat(usdt[0].quote.INR.price.toFixed(4));
       return USDTINR 
  }}
-  
-  //console.log(rate1(response1))
-  
-  
-}).catch((err) => {
-  console.log('error:', err.message);
-});
 
-rp("https://economictimes.indiatimes.com/markets/forex").then(response2 => {
+  console.log(rate1(response1))
+  return rp("https://economictimes.indiatimes.com/markets/forex")
+ 
+}).then(response2 => {
    //console.log('API call response:', response2)
   function rate2(response2){
       const dom = new JSDOM(response2)
       let USDINR = parseFloat(dom.window.document.querySelector('tr[class = "fRatesData active"]').children[1].textContent)
       return USDINR
   }
-
-  //console.log(rate2(response2))
- // expect(rate2(response2)).toBe(rate1(response1));
-
+  console.log(rate2(response2))
 
 }).catch((err) => {
   console.log('error:', err.message);
-});
+})
 
-
-async function testEquality() {
-  let [usdtinr,usdinr] = await Promise.all([rp(requestOptions),rp("https://economictimes.indiatimes.com/markets/forex")])
-  if (usdtinr > usdinr) {
-    return mail1
-  } else {
-    console.log("Tether<USD")
-  };
-}
-testEquality().catch(err => { console.log('Caught error', err); });
