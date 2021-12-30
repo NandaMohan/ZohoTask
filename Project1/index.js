6fcfd078c6235d218c6c6f3a70aecc15cd5c2bc1
NandaMohan <92676605+NandaMohan@users.noreply.github.com>
2021-12-30 20:46:03 +0530

    Update index.js

--- Project1/index.js
+++ Project1/index.js
@@ -1,87 +1,99 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+// App constants
+const port = process.env.PORT || 5000;
+const apiPrefix = '/api';
+
+// Store data in-memory, not suited for production use!
+const db = {
+	test: {
+	  userId : 1,
+	  person : 
+	  {
+		  name : 'Nanda',
+		  ph_no : 934932942
+	},
+	photo : 
+	{
+		lat : 12.8730300,
+		lng : 80.4332344
+	}
+	}
+  };
+// Create the Express app & setup middlewares
 const app = express();
-var bodyParser = require('body-parser')
-const db = require("./personphoto.json")
-const port = 3000
-const dist = require('./dist.js')
-const apiPrefix = '/api'  
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({ origin: /http:\/\/(127(\.\d){3}|localhost)/}));
+app.options('*', cors());
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended : true}))
-
+// Configure routes
 const router = express.Router();
 
-router.post('/upload' , (req,res) => {
-    const body = req.body
-    const name = body.name
-    const ph_no = body.ph_no
-    const lat = body.lat
-    const lng = body.lng
+router.post('/accounts', (req, res) => {
+  // Check mandatory request parameters
+  if (!req.body.userId ||!req.body.lat || !req.body.lng) {
+    return res.status(400).json({ error: 'Missing parameters' });
+  }
 
-    if (!body.lat || !body.lng) {
-        return res.status(400).json({ error: 'Missing parameters' });
-      }
+  // Check if account already exists
+  if (db[req.body.userId]) {
+    return res.status(409).json({ error: 'User already exists' });
+  }
 
-    if ((body.lat>90 ) || (body.lng>180)) {
-        return res.status(400).json({ error: 'Invalid parameters' });
-      }
-    
-    if (db[body.id]) {
-        return res.status(409).json({ error: 'User already exists' });
-      }
-    
-    if (db[body.lat] & db[body.lng]) {
-        return res.status(409).json({ error: 'Photo on the specified location already exists' });
-      }
-    
-      // Create account
-    const upload= {
-        person : 
-        {
-            name : body.name,
-            ph_no : body.ph_no
-        },
-        photo : 
-        {
-            lat : body.lat,
-            lng : body.lng
-        }
-      };
+  if ((req.body.lat>90 ) || (req.body.lng>180)) {
+	return res.status(400).json({ error: 'Invalid parameters' });
+  }
 
-      db[body.id] = upload
-      console.log(upload)
-      return res.status(201).json(upload);
-    })
+  const account = {
+    userId: req.body.userId,
+    person : {
+      name : req.body.name,
+      ph_no : req.body.ph_no
+    },
+    photo :
+    {
+        lat : req.body.lat,
+        lng : req.body.lng
+    }
+  };
+  db[req.body.userId] = account;
 
-router.get('/upload/:id' , (req,res) => {
-      const id = req.params.id
-      parseInt(db.id)
-      // if(!upload)
-      // {
-      //     return res.status(404)
-      //              .json({"error" : "User not found"})
-      // }
-      // return res.json(upload)
-  
-  })
+  return res.status(201).json(account);
+});
 
-router.delete('/upload/:id', (req, res) => {
-        const upload  = db[req.params.id];
-      
-        // Check if account exists
-        if (!upload) {
-          return res.status(404).json({ error: 'User does not exist' });
-        }
-      
-        // Removed account
-        delete db[req.params.id];
-      
-        res.sendStatus(204);
-      });
-      
+router.get('/accounts', (req, res) => {
+	return res.json(db);
+  });
+
+router.get('/accounts/:userId', (req, res) => {
+  const account = db[req.params.userId];
+  // Check if account exists
+  if (!account) {
+    return res.status(404).json({ error: 'User does not exist' });
+  }
+  return res.json(account);
+});
+
+// ----------------------------------------------
+
+// Remove specified account
+router.delete('/accounts/:userId', (req, res) => {
+  const account = db[req.params.userId];
+
+  // Check if account exists
+  if (!account) {
+    return res.status(404).json({ error: 'User does not exist' });
+  }
+
+  delete db[req.params.userId];
+
+  res.sendStatus(204);
+});
+
 app.use(apiPrefix, router);
 
-      // Start the server
- app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
-      });
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
