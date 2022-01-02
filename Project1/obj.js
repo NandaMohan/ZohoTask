e5c9fcaf39bfb62e72292ba4e5f357d6371babeb
NandaMohan <92676605+NandaMohan@users.noreply.github.com>
2022-01-02 20:00:10 +0530

    Add files via upload

---
+++ Project1/obj.js
@@ -0,0 +1,19 @@
+db = [
+    {
+	test : {
+	  userId : 0,
+	  person : 
+	  {
+		  name : 'Nanda',
+		  ph_no : 934932942
+	},
+	photo : 
+	{
+		lat : 12.8730300,
+		lng : 80.4332344
+	}
+	},
+}
+]
+
+module.exports = db
