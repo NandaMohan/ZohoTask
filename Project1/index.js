a4f6d11291858baabcb722c908a46f876c1e9ae9
NandaMohan <92676605+NandaMohan@users.noreply.github.com>
2022-01-04 20:44:14 +0530

    Update index.js

--- Project1/index.js
+++ Project1/index.js
@@ -72,7 +72,6 @@ router.get('/clear', function (req, res) {
 })
 
 router.post("/convert", function(req, res, next) {
-  console.log(req.body);
   if(typeof req.body.content == 'undefined' || req.body.content == null) {
       res.json(["error", "No data found"]);
   } else {
